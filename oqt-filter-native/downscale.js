fbc72bcc9a73ffbe178ec34bc3298a117bcca95f
Martin Raifer <martin.raifer@uni-heidelberg.de>
2017-02-24 14:46:38 +0100

    use all cpu cores while downscaling
    tile-reduce's queuing makes this unneccesary.

--- oqt-filter-native/downscale.js
+++ oqt-filter-native/downscale.js
@@ -19,8 +19,7 @@ tileReduce({
     mapOptions: {
         mbtilesPath: mbtilesPath,
         binningFactor: binningFactor
-    },
-    maxWorkers: Math.max(1, Math.floor(cpus/4)*4 - 1) // make sure the number of threads is not divisible by 4 to make sure that there's no congestion of tiles at a particular worker
+    }
 })
 .on('reduce', function(d) {
 })
