196d58a1cfdd4614a82bcc04f499ee83c828c647
Nathaniel Grudzinski <n.grudzinski@siu.edu>
2017-03-20 20:05:32 -0500

    Replaced hardcoding with variables

--- run.sh
+++ run.sh
@@ -10,12 +10,16 @@
 # * RESULTS_DIR - directory where resulting .mbtiles files are stored
 # * SERVER_SCRIPT - node script that serves the .mbtiles (assumed to be already
 #                   started with `forever`)
+# * SOURCE_FILE - mbtiles tiles file name to be used to extract features
+# * SOURCE_URL - URL to download SOURCE_FILE from 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # config
 WORKING_DIR=/mnt/data
 RESULTS_DIR=~/results
 SERVER_SCRIPT=/home/ubuntu/server/serve.js
+SOURCE_FILE=laos.mbtiles
+SOURCE_URL=https://s3.amazonaws.com/mapbox/osm-qa-tiles/latest.country/$SOURCE_FILE.gz
 
 # clean up
 trap cleanup EXIT
@@ -33,35 +37,35 @@ npm install --silent
 ./hotprojects.sh || true
 
 # download latest planet from osm-qa-tiles
-curl https://s3.amazonaws.com/mapbox/osm-qa-tiles/latest.planet.mbtiles.gz --silent | gzip -d > planet.mbtiles
+curl $SOURCE_URL --silent | gzip -d > $SOURCE_FILE
 
 # generate user experience data
-./experiences.sh planet.mbtiles
+./experiences.sh $SOURCE_FILE
 
 # generate osm-analytics data
 # buildings
-./crunch.sh planet.mbtiles buildings 64
+./crunch.sh $SOURCE_FILE buildings 64
 cp buildings.mbtiles $RESULTS_DIR/buildings.mbtiles.tmp
 rm $RESULTS_DIR/buildings.mbtiles -f
 mv $RESULTS_DIR/buildings.mbtiles.tmp $RESULTS_DIR/buildings.mbtiles
 forever restart $SERVER_SCRIPT
 rm buildings.mbtiles
 # highways
-./crunch.sh planet.mbtiles highways 32
+./crunch.sh $SOURCE_FILE highways 32
 cp highways.mbtiles $RESULTS_DIR/highways.mbtiles.tmp
 rm $RESULTS_DIR/highways.mbtiles -f
 mv $RESULTS_DIR/highways.mbtiles.tmp $RESULTS_DIR/highways.mbtiles
 forever restart $SERVER_SCRIPT
 rm highways.mbtiles
 # waterways
-./crunch.sh planet.mbtiles waterways 32
+./crunch.sh $SOURCE_FILE waterways 32
 cp waterways.mbtiles $RESULTS_DIR/waterways.mbtiles.tmp
 rm $RESULTS_DIR/waterways.mbtiles -f
 mv $RESULTS_DIR/waterways.mbtiles.tmp $RESULTS_DIR/waterways.mbtiles
 forever restart $SERVER_SCRIPT
 rm waterways.mbtiles
 # railways
-./crunch.sh planet.mbtiles railways 32
+./crunch.sh $SOURCE_FILE railways 32
 cp railways.mbtiles $RESULTS_DIR/railways.mbtiles.tmp
 rm $RESULTS_DIR/railways.mbtiles -f
 mv $RESULTS_DIR/railways.mbtiles.tmp $RESULTS_DIR/railways.mbtiles
